c05e57d4593c77b9013d68f0bcad23829d94ea24
Michael Gerard Ruddy <43761166+mgruddy@users.noreply.github.com>
2021-10-13 09:52:06 -0700

    Fixed issue loading cookbook for users who have no recipes

--- app/views.py
+++ app/views.py
@@ -39,8 +39,11 @@ def cookbook():
 			if recipe.id not in current_user.recipes:
 				current_user.recipes = current_user.recipes + [recipe.id]
 	db.session.commit()
-
-	recipes = [Recipes.query.get(id).to_dict() for id in current_user.recipes if id is not None][::-1]
+	
+	if not current_user.recipes:
+		recipes = []
+	else:
+		recipes = [Recipes.query.get(id).to_dict() for id in current_user.recipes if id is not None][::-1]
 	return render_template('cookbook.html', form=form, recipes=recipes)
 
 @app.route('/recipe/<id>', methods=['POST','GET'])
